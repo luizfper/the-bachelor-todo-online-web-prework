2a136b53632e7b93809fad031ff99f9e04b1d3fe
Luiz Pereira <luizfper@gmail.com>
2019-02-05 13:25:27 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,7 @@
 def get_first_name_of_season_winner(data, season)
   season_hash={}
-  season_hash=data.find {|key_season,value|key_season==season}
-  season_hash[season]["name"]
+  season_hash=data.find {|key_season,value|key_season==season}.flatten
+
 end
 
 def get_contestant_name(data, occupation)
