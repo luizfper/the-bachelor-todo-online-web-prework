020d6d9249f7a8032e741756ac206a9b4d6a3dd6
Luiz Pereira <luizfper@gmail.com>
2019-02-05 14:35:20 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -44,7 +44,7 @@ end
 def get_average_age_for_season(data, season)
   average_age=0
   counter=0
-  data.find {|key_season,value|key_season==season}[1]
+  data.find {|key_season,value|key_season==season}[0]
     # v_contestant.each do |contestant|
     #   if contestant["hometown"]==hometown
     #      occup=contestant["occupation"]
